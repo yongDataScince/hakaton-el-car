5cc941d7477ae3b5dd8432a212849d02323fc680
George <zhorenty@gmail.com>
2022-11-18 20:29:20 +0300

    asd

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -9,12 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home:
+    return MaterialApp.router(
+      theme: ThemeData.dark(),
     );
   }
 }
